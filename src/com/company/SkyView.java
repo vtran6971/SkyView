da511993e4f7e6d1b4243f94ea99096984ed2391
BT_1N3_15@bktech.local <BT_1N3_15@bktech.local>
2019-01-15 08:38:52 -0500

    Initial commit

---
+++ src/com/company/SkyView.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class SkyView {
+    private double[][] view;
+
+    public SkyView(int numRows, int numCols, double[] scanned)
+    {
+        view = new double[numRows][numCols];
+
+        for(int i = 0; i < numRows; i++) {
+            if(i%2 == )
+            for (int j = 0; j < numCols; j++) {
+
+            }
+        }
+
+    }
+}
