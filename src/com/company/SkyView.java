c7af4373546d87068d9fc48902a69c97767a04c0
Vincent Tran <vtran6971@bths.edu>
2019-01-15 22:15:53 -0500

    The transfer of data from 1d array to 2d array functions

--- src/com/company/SkyView.java
+++ src/com/company/SkyView.java
@@ -8,24 +8,39 @@ public class SkyView {
         view = new double[numRows][numCols];
         int index = 0;
 
-        for(int i = 0; i < numRows; i++) {
-            if(i%2 == 0)
-            {
-                for (int j = 0; j < numCols; j++)
-                {
-                    view[i][j] = scanned[index];
-                    index++;
+        //Checks if 2d array is large enough to collect data points from the 1d array
+        if(numRows*numCols == scanned.length) {
+                //Row increment
+                for (int i = 0; i < numRows; i++) {
+                    //Even rows
+                    if (i % 2 == 0) {
+                        //Column increment left to right
+                        for (int j = 0; j < numCols; j++) {
+                            view[i][j] = scanned[index++];
+                        }
+                    }
+
+                    //Odd rows
+                    if (i % 2 == 1) {
+                        //Column decrement right to left
+                        for (int j = numCols-1; j >= 0; j--) {
+                            view[i][j] = scanned[index++];
+                        }
+                    }
                 }
+            for (int i = 0; i<view.length; i++)
+            {
+                for (int j = 0; j < view[0].length; j++)
+                {
+                    System.out.print(view[i][j]+" ");
+                }
+                System.out.println("");
             }
 
-            if(i%2 == 1)
-            {
-                for (int j = numCols-1; j>0; j--)
-                {
-                    view[i][j] = scanned[index];
-                    index++;
-                }
-            }
+        }
+        else
+        {
+            System.out.println("Please increase 2D array rows or columns to increase array size!");
         }
     }
 }
