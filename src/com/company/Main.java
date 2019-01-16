9212ac80a91c84129a0543d85866445c8641e7cb
Vincent Tran <vtran6971@bths.edu>
2019-01-15 21:22:17 -0500

    Minor additions, not functioning

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
--- src/com/company/SkyView.java
+++ src/com/company/SkyView.java
@@ -6,13 +6,26 @@ public class SkyView {
     public SkyView(int numRows, int numCols, double[] scanned)
     {
         view = new double[numRows][numCols];
+        int index = 0;
 
         for(int i = 0; i < numRows; i++) {
-            if(i%2 == )
-            for (int j = 0; j < numCols; j++) {
+            if(i%2 == 0)
+            {
+                for (int j = 0; j < numCols; j++)
+                {
+                    view[i][j] = scanned[index];
+                    index++;
+                }
+            }
 
+            if(i%2 == 1)
+            {
+                for (int j = numCols-1; j>0; j--)
+                {
+                    view[i][j] = scanned[index];
+                    index++;
+                }
             }
         }
-
     }
 }
